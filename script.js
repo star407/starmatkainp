6e934226888967732f8b50559e91c8003d78efc7
star407 <starmatka786@gmail.com>
2023-09-25 16:59:59 +0530

    one

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ async function onsubmitclickln(str) {
      arr.push(...[parseInt(num%10)])
      num = parseInt(num/10)
     }
-    bodyy.ln = arr
+    bodyy.ln = arr.reverse()
    } else{
      //do nothing
      flag = false
