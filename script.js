bfd156efa2d85692657aa772dcd71fbb746e18e2
star407 <starmatka786@gmail.com>
2023-09-25 11:26:58 +0530

    twelve

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
-async function onsubmitclick(str) {
-    var bodyy = {
-        "title": str,
+async function onsubmitclick(str, patti) {
+  var flag = true
+  var bodyy = {
+    'title': str,
+  } 
+  const date = new Date();
+
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  // This arrangement can be altered based on how we want the date's format to
+  // appear.
+  let currentDate = `${year}-${month}-${day}`;
+  bodyy.entrydate = currentDate
+  if (patti === 'patti1') {
+    let arr = []
+    if (document.getElementById(`${str}patti1`).value != '') {
+     let num = parseInt(document.getElementById(`${str}patti1`).value)
+     while(num > 0){
+      arr.push(...[parseInt(num%10)])
+      num = parseInt(num/10)
+     }
+     if(document.getElementById(`${str}patti1num`).value != ''){
+      arr.push(...[parseInt(document.getElementById(`${str}patti1num`).value)])
+      arr.reverse()
+      bodyy.patti1 = arr
+     } else {
+      //do nothing
+      flag = false
+     }
+    } else{
+      //do nothing
+      flag = false
     }
-    if(document.getElementById(`${str}top`).value != ""){
-      bodyy.numbertop = document.getElementById(`${str}top`).value
+  } else {
+    let arr = []
+    if (document.getElementById(`${str}patti2`).value != '') {
+     let num = parseInt(document.getElementById(`${str}patti2`).value)
+     while(num > 0){
+      arr.push(...[parseInt(num%10)])
+      num = parseInt(num/10)
+     }
+     if(document.getElementById(`${str}patti2num`).value != ''){
+      arr.push(...[parseInt(document.getElementById(`${str}patti2num`).value)])
+      arr.reverse()
+      bodyy.updates = {
+        "patti2" : arr
+      }
+     } else {
+      //do nothing
+      flag = false
+     }
+    } else{
+      //do nothing
+      flag = false
     }
-    if(document.getElementById(`${str}middle`).value != ""){
-      bodyy.numbermiddle = document.getElementById(`${str}middle`).value
-    }
-    if(document.getElementById(`${str}bottom`).value != ""){
-      bodyy.numberbottom = document.getElementById(`${str}bottom`).value
-    }
-    document.getElementById(`${str}top`).value = ""
-    document.getElementById(`${str}middle`).value = ""
-    document.getElementById(`${str}bottom`).value = ""
+  }
+
+if(flag){
+  console.log(bodyy)
+  document.getElementById(`${str}${patti}`).value = ''
+  document.getElementById(`${str}${patti}num`).value = ''
+  if(patti === 'patti1'){
     await fetch(`https://starmatkaagain.onrender.com/upload`, {
         method: 'POST',
         headers: {
@@ -30,4 +78,23 @@ async function onsubmitclick(str) {
           .catch(error => {
             console.error('fetch error:', error);
           });
+  } else {
+    await fetch(`https://starmatkaagain.onrender.com/update`, { 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bodyy)
+      })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ozak');
+            }
+            return response.json();
+          })
+          .catch(error => {
+            console.error('fetch error:', error);
+          });
+  }
+}
 }
